2a4be720573292d2a00f8157b558df046244e386
Ayush Gupta <ayushgupta3902@gmail.com>
2024-01-03 02:04:14 +0530

    feat: complete live chat functionality

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,4 +1,21 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
+
+// pub stands for publisher
+const pub = new Redis({
+  host: "redis-23be96a7-ayushforwork390-77fc.a.aivencloud.com",
+  port: 14580,
+  username: "default",
+  password: "AVNS_ijyouc63CoVkadvy2r_",
+});
+
+// sub stands for subscriber
+const sub = new Redis({
+  host: "redis-23be96a7-ayushforwork390-77fc.a.aivencloud.com",
+  port: 14580,
+  username: "default",
+  password: "AVNS_ijyouc63CoVkadvy2r_",
+});
 
 class SocketService {
   private _io: Server;
@@ -6,11 +23,12 @@ class SocketService {
   constructor() {
     console.log("Init Socket server...");
     this._io = new Server({
-        cors: {
-            allowedHeaders: ["*"],
-            origin: "*",
-        }
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
     });
+    sub.subscribe("MESSAGES")
   }
 
   public initListeners() {
@@ -21,8 +39,16 @@ class SocketService {
 
       socket.on("event:message", async ({ message }: { message: string }) => {
         console.log(`New Message received: ${message}`);
+        // publish this message to redis
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
       });
     });
+
+    sub.on("message", (channel, message) => {
+        if (channel === 'MESSAGES') {
+            io.emit("message", message);
+        }
+    })
   }
 
   get io() {
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,14 +4,10 @@ import { useSocket } from "../context/SocketProvider";
 import classes from "./page.module.css";
 
 export default function Page() {
-  const { sendMessage } = useSocket();
+  const { sendMessage, messages } = useSocket();
   const [message, setMessage] = useState("");
   return (
     <div>
-      <div>
-        <h1>All Messages</h1>
-      </div>
-
       <div>
         <input
           onChange={(e) => setMessage(e.target.value)}
@@ -24,6 +20,11 @@ export default function Page() {
         >
           Send
         </button>
+      </div>
+      <div>
+        {messages.map((e, i) => (
+          <li key={i}>{e}</li>
+        ))}
       </div>
     </div>
   );
